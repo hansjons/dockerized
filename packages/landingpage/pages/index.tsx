1614cd5b808aea50bbfadad15f2db8efc1b53a9c
Hans Pétur Jónsson <hans@aha.is>
2021-04-12 22:49:59 +0000

    Update index.tsx

--- packages/landingpage/pages/index.tsx
+++ packages/landingpage/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
 
-const IndexPage = () => <h1>Welcome to the Dockerized App and coffee!</h1>
+const IndexPage = () => <h1>Welcome to the Dockerized App and coffee more coffee!</h1>
 
 export default IndexPage
